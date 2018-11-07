bd40e7ae2ab5eda483c3a23658b7924d0d45a1af
Matt Kerbel <kerbel.matt@gmail.com>
2018-11-07 01:34:05 -0500

    Added database helper class

---
+++ HenryBooks/modDB.vb
@@ -0,0 +1,7 @@
+﻿Imports System.Data.SqlClient
+
+Module modDB
+    Public objSQLConn As SqlConnection
+    Public objSQLCommand As SqlCommand
+    Public gstrConn As String = "Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFileName=D:\OneDrive\School\Windows Programming for IT\HenryBooks\HBooks_2017.mdf;Integrated Security=True"
+End Module
